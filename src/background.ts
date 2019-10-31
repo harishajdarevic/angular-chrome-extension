29a2ae3d93f42ab49655854e7a7296c29d60d327
Haris Hajdarevic <haris.hajdarevic@innovic.io>
2019-10-31 10:14:08 +0100

    Initial commit

---
+++ src/background.ts
@@ -0,0 +1,8 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.webNavigation.onCompleted.addListener(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+      chrome.pageAction.show(id);
+    });
+  }, { url: [{ urlMatches: 'olx.ba' }] });
+});
+
